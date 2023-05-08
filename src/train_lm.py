e29b6f1312f1043af71560d894ef790e1725dce6
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2023-05-08 16:18:20 -0700

    tokenizer training complete

--- src/train_lm.py
+++ src/train_lm.py
@@ -7,7 +7,7 @@ from omegaconf import DictConfig, OmegaConf
 
 @hydra.main(config_path='../config', config_name='main', version_base=None)
 def train_lm(cfg: DictConfig) -> None:
-    pass
+    print(OmegaConf.to_yaml(cfg))
 
 if __name__ == '__main__':
     train_lm()
