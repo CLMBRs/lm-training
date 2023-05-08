38856128a260e0b57adf631d0f3341b0d2732884
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2023-05-08 13:55:33 -0700

    basic infra for hydra

---
+++ src/train.py
@@ -0,0 +1,13 @@
+"""
+Primary script for using HuggingFace Trainer to train a language model.
+"""
+
+import hydra
+from omegaconf import DictConfig, OmegaConf
+
+@hydra.main(config_path='../config', config_name='main', version_base=None)
+def train_lm(cfg: DictConfig) -> None:
+    pass
+
+if __name__ == '__main__':
+    train_lm()
