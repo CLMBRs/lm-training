08118252570615e5346afa7d88c0e33ac0dd6715
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2023-05-11 10:49:40 -0700

    instantiate opt config

--- src/train_lm.py
+++ src/train_lm.py
@@ -9,7 +9,13 @@ from omegaconf import DictConfig, OmegaConf
 @hydra.main(config_path="../config", config_name="train-lm", version_base=None)
 def train_lm(cfg: DictConfig) -> None:
     print(OmegaConf.to_yaml(cfg))
-    tokenizer = hydra.utils.call(cfg.tokenizer)
+    # build tokenizer
+    tokenizer = hydra.utils.instantiate(cfg.tokenizer)
+    # get config first, then instantiate model
+    # this way, can set vocab size via the tokenizer
+    config = hydra.utils.instantiate(cfg.model.config)
+    config.vocab_size = tokenizer.get_vocab_size()
+    print(config)
 
 
 if __name__ == "__main__":
