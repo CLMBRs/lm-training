b197ecc75bc429dc6b46fa72b1d415ed31a33e59
Jon Davenport <jonathan.m.davenport@gmail.com>
2024-09-09 15:43:40 -0700

    updated src/model/custom_electra/electra.py

--- src/model/custom_electra/electra.py
+++ src/model/custom_electra/electra.py
@@ -13,23 +13,18 @@ class CustomElectraConfig(PretrainedConfig):
 
     def __init__(
         self,
-        generator_model: transformers.DebertaV2Config,
-        discriminator_model: transformers.DebertaV2Config,
+        generator_config: PretrainedConfig,
+        discriminator_config: PretrainedConfig,
         vocab_size: int,
         embedding_sharing: bool= True,
         discriminator_lambda_loss: float= 50.0, 
         pretrained_model_name_or_path= None,
         **kwargs,
     ):  
-        print("===== kwargs =====")
-        for key, value in kwargs.items():
-            print(f"{key}: {value}")
-        print(f'pretraineds_model_name_or_path: {pretrained_model_name_or_path}')
-        #super().__init__(pretrained_model_name_or_path)
         super().__init__(**kwargs)
         self.vocab_size= vocab_size
-        self.generator_config= generator_model
-        self.discriminator_config= discriminator_model
+        self.generator_config= generator_config
+        self.discriminator_config= discriminator_config
         self.embedding_sharing= embedding_sharing
         self.discriminator_lambda_loss= discriminator_lambda_loss
         self._name_or_path= pretrained_model_name_or_path
@@ -41,7 +36,6 @@ class CustomElectraConfig(PretrainedConfig):
 class CustomElectraModel(PreTrainedModel):
 
     # loss weights are default from paper
-    #def __init__(self, config: AutoConfig, tokenizer: PretrainedTokenizerFast=None, lambda_loss=50.0):
     
     config_class = CustomElectraConfig
 
@@ -54,13 +48,11 @@ class CustomElectraModel(PreTrainedModel):
 
         # can change this to pass the configs,
         # or pass the generator/discriminators themselves
-        #base_gen_config = ElectraConfig.from_pretrained('google/electra-base-generator')
         base_gen_config= AutoConfig.from_pretrained(config.generator_config)
-        self.generator: ElectraForMaskedLM = AutoModelForMaskedLM.from_config(base_gen_config)
+        self.generator: AutoModelForMaskedLM = AutoModelForMaskedLM.from_config(base_gen_config)
 
-        #base_disc_config = ElectraConfig.from_pretrained('google/electra-base-discriminator')
         base_disc_config = AutoConfig.from_pretrained(config.discriminator_config)
-        self.discriminator: ElectraForPreTraining = AutoModelForMaskedLM.from_config(base_disc_config)
+        self.discriminator: AutoModelForTokenClassification = AutoModelForTokenClassification.from_config(base_disc_config)
 
         # we can modify src/train_tokenizer.py to take different tokenizer classes
         # and train it there, pass it here
@@ -71,7 +63,7 @@ class CustomElectraModel(PreTrainedModel):
                 input_ids: Optional[torch.Tensor] = None, 
                 attention_mask: Optional[torch.Tensor] = None, 
                 token_type_ids: Optional[torch.Tensor] = None, 
-                labels: Optional[torch.Tensor] = None) -> ElectraForPreTrainingOutput:
+                labels: Optional[torch.Tensor] = None) -> TokenClassifierOutput:
         '''
         labels (`torch.Tensor` of shape `(batch_size, sequence_length)`):
             Labels indicating whether or not the token was replaced (used for computing loss).
@@ -99,7 +91,7 @@ class CustomElectraModel(PreTrainedModel):
         # loss
         loss = generator_output.loss + discriminator_output.loss * self.lambda_loss #might have to rewrite as loss= generator_output.loss.loss + discriminator_output.loss.loss * self.lambda_loss or it might be that discriminator_output.loss.loss is the only one that needs to be .loss twice
         # must return some output.loss in order to work with trainer
-        return ElectraForPreTrainingOutput(
+        return TokenClassifierOutput(
             loss=loss,
             logits=discriminator_output.logits,
             hidden_states=discriminator_output.hidden_states,
@@ -110,5 +102,3 @@ class CustomElectraModel(PreTrainedModel):
     def sample(self, logits):
         gumbel_output = self.gumbel_dist.sample(logits.shape)
         return (logits.float() + gumbel_output).argmax(dim=-1)
-
-
